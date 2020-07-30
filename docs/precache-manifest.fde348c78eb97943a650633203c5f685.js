b6da42c2cb5966a8f116cb0f0ef33a609ed531d0
jl-robles <jlrobles0720@gmail.com>
2020-07-30 12:46:10 -0500

    docs added

---
+++ docs/precache-manifest.fde348c78eb97943a650633203c5f685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5d4e57cadad35c2f0c6cfe360d50e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f0ba5884749f656d70",
+    "url": "/static/css/main.25804a12.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f0ba5884749f656d70",
+    "url": "/static/js/main.121fbf8f.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
